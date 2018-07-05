3e990a3b8bce696ece9bd5cb2168fa7d9420155b
Jithendra Thenuwara <jithendrathenu@gmail.com>
2018-07-05 21:19:22 +0530

    Add files via upload

---
+++ src/app/app.config.ts
@@ -0,0 +1,2 @@
+export const MAP_API_KEY='AIzaSyDhQQoh1RqlECY0LB6gtmA-FbjGukdVhOM';
+export const SERVICE_URL='https://kaemauat.azurewebsites.net/service';
---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG ={
+	
+	apiKey: "AIzaSyCei7NMvcUZ4365QYzRk45tolqwMC969po",
+    authDomain: "kaema-uat.firebaseapp.com",
+    databaseURL: "https://kaema-uat.firebaseio.com/",
+    projectId: "kaema-uat",
+    storageBucket: "gs://kaema-uat.appspot.com",
+    messagingSenderId: "254041655268"
+}
