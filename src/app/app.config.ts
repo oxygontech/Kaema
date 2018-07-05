c0b53a570dfb2f0f7c81e7cd1f1c7e0650c55afe
Jithendra Thenuwara <jithendrathenu@gmail.com>
2018-07-05 21:09:19 +0530

    Dev master (#4)
    * Moved API and Service url to seperate configuration file
    
    * Prevented version control on config files
    
    * Prevented version control on config files
    
    * Removed config files from versioning

--- src/app/app.config.ts
+++
@@ -1,2 +0,0 @@
-export const MAP_API_KEY='AIzaSyAU0DykFHNFk4J8LpNbQGGZbZVXsAf83Cs';
-export const SERVICE_URL='https://kaema.azurewebsites.net/service';
--- src/app/app.firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CONFIG ={
-	
-	apiKey: "AIzaSyB9UDSLBe7K9VfMeieIgwIK5SqkL8KFmQ4",
-    authDomain: "kaema-af14c.firebaseapp.com",
-    databaseURL: "https://kaema-af14c.firebaseio.com",
-    projectId: "kaema-af14c",
-    storageBucket: "gs://kaema-af14c.appspot.com",
-    messagingSenderId: "983666357627"
-}
